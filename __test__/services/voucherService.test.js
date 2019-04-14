0537e60951fd8c33fa7e3a9ff347d8019bd818c0
Malachy Gilchrist <malachygilchrist@gmail.com>
2019-04-15 00:39:09 +0100

    voucherService checks minimum spend for voucher is met

--- __test__/services/voucherService.test.js
+++ __test__/services/voucherService.test.js
@@ -1,10 +1,21 @@
 const voucherService = require('../../services/voucherService')
 
 describe('.applyVoucher', () => {
-    test('returns the cart total with discount applied', (done) => {
-        const cartTotal = 198
-        const voucherCode = 'MONEYOFF'
-        expect(voucherService.applyVoucher(voucherCode, cartTotal)).toEqual(193)
-        done()
+    describe('when voucher is valid', () => {
+        test('returns the cart total with discount applied', (done) => {
+            const cartTotal = 198
+            const voucherCode = 'MONEYOFF'
+            expect(voucherService.applyVoucher(voucherCode, cartTotal)).toEqual(193)
+            done()
+        })
+    })
+
+    describe('when voucher is invalid', () => {
+        test('doen not apply any discounts', (done) => {
+            const cartTotal = 10
+            const voucherCode = 'MOREMONEYOFF'
+            expect(voucherService.applyVoucher(voucherCode, cartTotal)).toEqual(10)
+            done()
+        })
     })
 })
--- services/voucherService.js
+++ services/voucherService.js
@@ -3,10 +3,18 @@ const discounts = require('../public/deals.json')
 applyVoucher = (voucherCode, cartTotal) => {
     for (var i=0; i<discounts.deals.length; i++) {
         if (discounts.deals[i].code === voucherCode) {
-            return cartTotal - discounts.deals[i].discount
+            if (isValidVoucher(discounts.deals[i], cartTotal)) {
+                return cartTotal - discounts.deals[i].discount
+            }
         }
     }
     return cartTotal
+}
+
+isValidVoucher = (deal, cartTotal) => {
+    if (deal.conditions["min-spend"] < cartTotal) {
+        return true
+    }
 }
 
 module.exports = {
